f6b9cace05bbf0dcd574aa7babbd2446410af0d4
Jaya Jain <jayajn98@gmail.com>
2020-09-23 18:31:02 +0530

    UC-1 : EmployeeAttendanceUC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,14 @@
 #displayStart
 echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM"
+#checkAttendance
+for ((numOfEmployees=0; numOfEmployees<=5; numOfEmployees++))
+do
+  var=$((RANDOM%50+1))
+  read -p " Is Employee number $var 1.Present OR 2.Absent ? : " Attendance
+  if [ $Attendance -eq 1 ]
+     then 
+        echo "Employee number $var is Present"
+  else	
+	echo "Employee number $var is Absent"
+  fi
+done 
