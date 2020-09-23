8d22d4c410c8140e4b69ae809aca6bf0af737045
Jaya Jain <jayajn98@gmail.com>
2020-09-24 03:15:29 +0530

    UC-5 : CalculateWagesForMonthUC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -16,11 +16,15 @@ done
 dailyWage=`expr 20 \* 8`
 echo "Daily employee wage is $dailyWage"
 #partTimeEmployee&Wage
-partTimeWage=`expr dailyWage + 20 \* 8`
+partTimeWage=`expr $dailyWage + 20 \* 8`
 echo "After adding part time wage, the Total wage is $partTimeWage"
 #solving using switch cases
-read -p "how do you want to calculate the wage ? : " choice
+read -p "how do you want to calculate the wage ? 1.Monthly OR 2.HourlyORDays : " choice
 case $choice in 
 1) echo "Calculating using month"
+   monthWage=`expr 20 \* 20 \* 8`
+   echo "Monthly wage for 20 working days is $monthWage"
 ;;
-2) echo "Calculating using hours and month" 
+2) echo "Calculating using hours and month"
+;;
+esac 
